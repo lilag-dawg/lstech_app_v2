3bc7e9808248724c94742066e02a0387bca51344
alexis <alexis.garant@gmail.com>
2021-01-13 09:53:33 -0500

    added supported datatype but couldnt test it

--- lstech_app/lib/models/myBluetoothDevice.dart
+++ lstech_app/lib/models/myBluetoothDevice.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:lstech_app/models/recognizedData.dart';
 import './bluetoothDeviceService.dart';
 
 class MyBluetoothDevice {
   final BluetoothDevice device;
+
+  List<DataType> supportedDataType;
+
   List<BluetoothDeviceService> services = [];
 
   MyBluetoothDevice._create(this.device);
@@ -20,8 +24,25 @@ class MyBluetoothDevice {
               s.uuid.toString().toUpperCase().substring(4, 8), s)
           .then((onValue) {
         services.add(onValue);
+        _setSupportedDataType(onValue);
       });
     });
+  }
+
+  void _setSupportedDataType(BluetoothDeviceService service) {
+    // ce n'est pas encore testé
+    if (service.name == "1818") {
+      supportedDataType.add(DataType.power);
+    }
+    if (service.name == "1816") {
+      supportedDataType.add(DataType.power);
+    }
+    if (service.name == "180F") {
+      supportedDataType.add(DataType.battery);
+    }
+    if (service.name == "180D") {
+      supportedDataType.add(DataType.cardiac);
+    }
   }
 
   BluetoothDeviceService getService(String name) {
