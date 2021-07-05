56e25392426f57037fb11850789c94cf2fb1a891
alexis <alexis.garant@gmail.com>
2021-07-05 16:17:53 -0400

    published to google testers

--- lstech_app/lib/main.dart
+++ lstech_app/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'LsTech+ App',
+        title: 'LsTech+ Demo',
         home: NavigationScreen(),
         debugShowCheckedModeBanner: false,
       ),
